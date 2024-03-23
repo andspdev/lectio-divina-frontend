9038e3d66e9dfa4725aa2e89847af1307d458b35
Andreaas Pandu P <andspemail@gmail.com>
2024-03-23 13:05:20 +0700

    Update halaman not found pathname

--- src/components/errors/NotFound.js
+++ src/components/errors/NotFound.js
@@ -2,10 +2,11 @@ import { useContext, useEffect } from 'react'
 import { Context } from '../../includes/GlobalState'
 
 import ImageInternalServerError from '../../assets/images/svg/maintenance-illustration.svg'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 const NotFound = () =>
 {
+    const { pathname } = useLocation()
     const [ stateGlobal ] = useContext(Context)
     
     useEffect(() =>
@@ -19,7 +20,7 @@ const NotFound = () =>
             <h3 className='title'>Halaman Tidak Ditemukan</h3>
 
             <div className='mt-4'>
-                <p>Halaman yang Anda cari tidak dapat ditemukan atau sedang dalam perbaikan.</p>
+                <p>Halaman yang Anda cari <i>{pathname}</i> tidak dapat ditemukan atau sedang dalam perbaikan.</p>
 
                 <Link to="/" className='btn btn-primary btn-primary-ld'>Kembali ke Beranda</Link>
             </div>
--- src/components/errors/NotFoundUserLogin.js
+++ src/components/errors/NotFoundUserLogin.js
@@ -2,10 +2,11 @@ import { useContext, useEffect } from 'react'
 import { Context } from '../../includes/GlobalState'
 
 import ImageInternalServerError from '../../assets/images/svg/maintenance-illustration.svg'
-import { useOutletContext } from 'react-router-dom';
+import { useLocation, useOutletContext } from 'react-router-dom';
 
 const NotFoundUserLogin = () =>
 {
+    const { pathname } = useLocation()
     const [, setTitle, , setMenuSelected ] = useOutletContext();
     const [ stateGlobal ] = useContext(Context)
     
@@ -24,7 +25,7 @@ const NotFoundUserLogin = () =>
             <h3 className='title'>Halaman Tidak Ditemukan</h3>
 
             <div className='mt-4'>
-                <p>Halaman yang Anda cari tidak dapat ditemukan atau sedang dalam perbaikan.</p>
+                <p>Halaman yang Anda cari <i>{pathname}</i> tidak dapat ditemukan atau sedang dalam perbaikan.</p>
             </div>
         </div>
     )
